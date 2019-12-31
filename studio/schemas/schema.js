d1931c6a2438cc19f7e05aff37a7c1cca5b1f5d3
Jacob D. Castro <jdcastro.business@gmail.com>
2019-12-30 18:09:33 -0800

    add top-level objects

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -4,13 +4,15 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 // documents
 import blogPost from './documents/blogPost';
 import product from './documents/product';
-// import comic from './documents/comic';
+import comic from './documents/comic';
 // import siteSettings from './documents/siteSettings';
 
 // objects
 import imageWithMeta from './objects/imageWithMeta';
+import comicAct from './objects/comicAct';
+import comicScene from './objects/imageWithMeta';
 
 export default createSchema({
   name: 'captain-senpy-schema',
-  types: schemaTypes.concat([blogPost, product, imageWithMeta]),
+  types: schemaTypes.concat([blogPost, product, imageWithMeta, comicAct, comicScene]),
 });
