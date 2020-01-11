9c960559ba319120f7576353193e36c16b084e50
Jacob D. Castro <jdcastro.business@gmail.com>
2020-01-10 19:19:28 -0800

    configure products and pages

--- studio/lib/deskStructure.js
+++ studio/lib/deskStructure.js
@@ -12,8 +12,11 @@ const hiddenDocTypes = listItem =>
     'comicAct',
     'comicEpisode',
     'product',
-    'sitePage',
     'siteSettings',
+    'indexPage',
+    'blogPage',
+    'comicsPage',
+    'storePage',
   ].includes(listItem.getId());
 
 export default () =>
@@ -26,31 +29,52 @@ export default () =>
         .schemaType('blogPost')
         .child(S.documentTypeList('blogPost').title('Blog posts')),
 
-      // S.listItem()
-      //   .title('Site Pages')
-      //   .icon(MdLayers)
-      //   .child(
-      //     S.listItem()
-      //       .title('Home Page')
-      //       .icon(MdLayers)
-      //       .schemaType('indexPageData')
-      //       .child(S.documentTypeList('indexPageData').title('Home Page')),
-      //     S.listItem()
-      //       .title('Blog Page')
-      //       .icon(MdLayers)
-      //       .schemaType('blogPageData')
-      //       .child(S.documentTypeList('blogPageData').title('Blog Page')),
-      //     S.listItem()
-      //       .title('Comics Page')
-      //       .icon(MdLayers)
-      //       .schemaType('comicsPageData')
-      //       .child(S.documentTypeList('comicsPageData').title('Comics Page')),
-      //     S.listItem()
-      //       .title('Store Page')
-      //       .icon(MdLayers)
-      //       .schemaType('storePageData')
-      //       .child(S.documentTypeList('storePageData').title('Store Page'))
-      //   ),
+      S.listItem()
+        .title('Site Pages')
+        .icon(MdLayers)
+        .child(
+          S.list()
+            .title('Site Pages')
+            .showIcons(false)
+            .items([
+              S.listItem()
+                .title('Home Page')
+                .child(
+                  S.editor()
+                    .title('Edit Home Page')
+                    .id('indexPage')
+                    .schemaType('indexPage')
+                    .documentId('indexPage')
+                ),
+              S.listItem()
+                .title('Blog Page')
+                .child(
+                  S.editor()
+                    .title('Edit Home Blog Page')
+                    .id('blogPage')
+                    .schemaType('blogPage')
+                    .documentId('blogPage')
+                ),
+              S.listItem()
+                .title('Comics Page')
+                .child(
+                  S.editor()
+                    .title('Edit Comics Page')
+                    .id('comicsPage')
+                    .schemaType('comicsPage')
+                    .documentId('comicsPage')
+                ),
+              S.listItem()
+                .title('Store Page')
+                .child(
+                  S.editor()
+                    .title('Edit Store Page')
+                    .id('storePage')
+                    .schemaType('storePage')
+                    .documentId('storePage')
+                ),
+            ])
+        ),
 
       S.listItem()
         .title('Authors')
@@ -66,12 +90,8 @@ export default () =>
       S.listItem()
         .title('Store')
         .icon(MdShoppingCart)
-        .child(
-          S.listItem()
-            .title('Products')
-            .schemaType('product')
-            .child(S.documentTypeList('product').title('Products'))
-        ),
+        .schemaType('product')
+        .child(S.documentTypeList('product').title('Products')),
 
       ...S.documentTypeListItems().filter(hiddenDocTypes),
 
