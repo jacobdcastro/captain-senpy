8b0a4ec563f145d661c584f08ebbe1e7c6dc031c
Jacob D. Castro <jdcastro.business@gmail.com>
2020-01-10 19:20:00 -0800

    add schemas for each main page

---
+++ studio/models/documents/pages/blogPage.js
@@ -0,0 +1,59 @@
+// import { format } from 'date-fns';
+
+export default {
+  name: 'blogPage',
+  type: 'document',
+  title: 'Blog Page',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Page Title',
+      description: 'Page title that appears in search results and in the browser tab.',
+    },
+    {
+      name: 'mainImage',
+      type: 'mainImage',
+      title: 'Main image',
+      description: 'Will default to image in Site Settings if no image image is placed here.',
+    },
+    {
+      name: 'description',
+      type: 'excerptPortableText',
+      title: 'Page Description',
+      description:
+        'This ends up on summary pages, on Google, when people share your post in social media.',
+    },
+    {
+      name: 'body',
+      type: 'bodyPortableText',
+      title: 'Body Content',
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [{ type: 'string' }],
+      options: {
+        layout: 'tags',
+      },
+    },
+  ],
+  // preview: {
+  //   select: {
+  //     title: 'title',
+  //     publishedAt: 'publishedAt',
+  //     slug: 'slug',
+  //     media: 'mainImage',
+  //   },
+  //   prepare({ title = 'No title', publishedAt, slug = {}, media }) {
+  //     const dateSegment = format(publishedAt, 'YYYY/MM');
+  //     const path = `/${dateSegment}/${slug.current}/`;
+  //     return {
+  //       title,
+  //       media,
+  //       subtitle: publishedAt ? path : 'Missing publishing date',
+  //     };
+  //   },
+  // },
+};
