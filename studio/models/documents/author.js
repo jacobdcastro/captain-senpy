1618f3dec46b37619fc520fcf4c93263993f8f5a
Jacob D. Castro <jdcastro.business@gmail.com>
2020-01-10 19:21:12 -0800

    remove slug from author

--- studio/models/documents/author.js
+++ studio/models/documents/author.js
@@ -9,16 +9,6 @@ export default {
       title: 'Name',
     },
     {
-      name: 'slug',
-      type: 'slug',
-      title: 'Slug',
-      description: 'Some frontends will require a slug to be set to be able to show the person',
-      options: {
-        source: 'name',
-        maxLength: 96,
-      },
-    },
-    {
       name: 'image',
       type: 'mainImage',
       title: 'Image',
