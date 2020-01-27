6ff675f984e79c02ea0219439acd4885ae7f41db
Jacob D. Castro <jdcastro.business@gmail.com>
2020-01-26 16:48:28 -0800

    basic repo setup

--- www/src/components/layout.js
+++ www/src/components/layout.js
@@ -1,33 +1,15 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
 
-import SEO from '../components/seo';
-import Header from './header';
+import SEO from './seo';
+import Header from './Header';
 import './layout.css';
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
     <>
       <SEO title="Home" />
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header siteTitle="Captain Senpy" />
       <div
         style={{
           margin: `0 auto`,
