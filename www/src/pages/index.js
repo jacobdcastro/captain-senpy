4679c6ba8a3831ff61d123a2b6ef03d87f2ebd54
Jacob D. Castro <jdcastro.business@gmail.com>
2020-01-26 16:47:58 -0800

    basic page component config

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -1,19 +1,42 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
+import BlockContent from '../components/block-content';
+import Layout from '../components/Layout';
 
-import Layout from '../components/layout';
-import Image from '../components/image';
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+      <Link to="/page-2/">Go to page 2</Link>
+      <BlockContent blocks={data._rawBody} />
+    </Layout>
+  );
+};
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-);
+IndexPage.propTypes = {
+  data: PropTypes.object.isRequired,
+};
 
 export default IndexPage;
+
+export const INDEX_PAGE_QUERY = graphql`
+  query INDEX_PAGE_QUERY {
+    allSanityBlogPost {
+      edges {
+        node {
+          id
+          title
+          slug {
+            current
+          }
+          publishedAt
+          _rawBody
+        }
+      }
+    }
+  }
+`;
