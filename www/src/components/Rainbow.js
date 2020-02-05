b668b24527cc4c8cd184854b9c915661428b82bb
Jacob D. Castro <jdcastro.business@gmail.com>
2020-02-04 19:58:40 -0800

    begin rainbow animation

---
+++ www/src/components/Rainbow.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/rainbow.scss';
+
+const Rainbow = () => {
+  return (
+    <div className="rainbow">
+      <div class="arc arc-red"></div>
+      <div class="arc arc-orange"></div>
+      <div class="arc arc-yellow"></div>
+      <div class="arc arc-green"></div>
+      <div class="arc arc-blue"></div>
+      <div class="arc arc-purple"></div>
+      <div class="arc arc-transparent"></div>
+    </div>
+  );
+};
+
+export default Rainbow;
--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Link, graphql } from 'gatsby';
 import BlockContent from '../components/block-content';
 import Layout from '../components/Layout';
+import Rainbow from '../components/Rainbow';
 
 const IndexPage = ({ data }) => {
   const postData = data.sanityBlogPost;
@@ -13,6 +14,8 @@ const IndexPage = ({ data }) => {
       <p>Now go build something great.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
       <Link to="/page-2/">Go to page 2</Link>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+      <Rainbow />
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
       <div>
         <h2>{postData.title}</h2>
