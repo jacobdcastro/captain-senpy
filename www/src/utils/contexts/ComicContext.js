caa63c00fb52e217af4f4c90e5781eaf2d962054
Jacob D. Castro <jdcastro.business@gmail.com>
2020-03-03 20:22:47 -0800

    add comic context to site

--- www/src/utils/contexts/ComicContext.js
+++ www/src/utils/contexts/ComicContext.js
@@ -3,9 +3,37 @@ import { useStaticQuery, graphql } from 'gatsby';
 import { ComicContext } from './index';
 
 const ComicContextParent = ({ children }) => {
-  const [comicData, setComicData] = useState([]);
-  // const { data } = useStaticQuery(graphql``);
-  return <ComicContext.Provider>{children}</ComicContext.Provider>;
+  const data = useStaticQuery(graphql`
+    query COMIC_VIEWER_QUERY {
+      allSanityAct {
+        edges {
+          node {
+            _id
+            title
+            episodes {
+              title
+              comicImg {
+                alt
+                asset {
+                  id
+                  fluid {
+                    ...GatsbySanityImageFluid_withWebp
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  const acts = data.allSanityAct.edges;
+  let allEpisodes = [];
+  acts.forEach(act => act.node.episodes.forEach(ep => allEpisodes.push(ep)));
+
+  console.log(allEpisodes);
+
+  return <ComicContext.Provider value={allEpisodes}>{children}</ComicContext.Provider>;
 };
 
 export default ComicContextParent;
--- www/src/utils/contexts/index.js
+++ www/src/utils/contexts/index.js
@@ -3,25 +3,4 @@ import { createContext } from 'react';
 // context is string of blog post id
 export const BlogContext = createContext('');
 
-export const ComicContext = createContext([
-  {
-    id: 1245678,
-    episodes: [
-      {
-        id: 124567,
-        finished: true,
-        active: false,
-      },
-    ],
-  },
-  {
-    id: 1245678,
-    episodes: [
-      {
-        id: 124567,
-        finished: false,
-        active: true,
-      },
-    ],
-  },
-]);
+export const ComicContext = createContext(null);
