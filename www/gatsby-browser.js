877dda68800175d80a814eb5f46df60bdca0ec8b
Jacob D. Castro <jdcastro.business@gmail.com>
2020-03-07 17:02:05 -0800

    add blog context to wrapper

--- www/gatsby-browser.js
+++ www/gatsby-browser.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import ComicContextParent from './src/utils/contexts/ComicContext';
+import BlogContextParent from './src/utils/contexts/BlogContext';
 
-export const wrapRootElement = ({ element }) => <ComicContextParent>{element}</ComicContextParent>;
+export const wrapRootElement = ({ element }) => (
+  <ComicContextParent>
+    <BlogContextParent>{element}</BlogContextParent>
+  </ComicContextParent>
+);
