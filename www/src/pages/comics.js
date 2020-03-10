37912eac67d7c114a3b659a64fb0994fbd64d1f2
Jacob D. Castro <jdcastro.business@gmail.com>
2020-03-10 16:49:43 -0700

    change layout naming

--- www/src/pages/comics.js
+++ www/src/pages/comics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Layout from '../components/layout/Layout';
+import Layout from '../components/layout';
 import ComicViewer from '../components/comic/ComicViewer';
 
 const ComicsPage = props => {
